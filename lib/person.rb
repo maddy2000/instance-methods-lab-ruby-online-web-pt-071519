2e3c1ad10d8877fcbdf5ef0152eb1ad6360cb70e
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-21 02:34:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,9 @@
 class Person
-  def talk to your Person
+  def talk 
     puts "Hello World!"
-    ada = Person.new
-    ada.talk
-  def walk to your Person
+  end
+  def walk
     puts "The Person is walking"
-    ada = Person.new
-    ada walk
-end
-end 
+  end 
 end
 
